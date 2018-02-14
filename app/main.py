576d04afc9a899283ff860b963a2cb03c641bbe1
ruchi-shekar <rshekar618@gmail.com>
2018-02-14 16:26:27 -0600

    adding to main.py

--- app/main.py
+++ app/main.py
@@ -12,16 +12,14 @@ import githubstats
 APP = flask.Flask(__name__)
 
 @APP.route('/')
-def hello():
-    return "hello world"
-
-@APP.route('/home')
 def home():
     return flask.render_template('home.html')
 
-# @APP.route('/coffeeshops')
-# def home():
-#     return flask.render_template('coffeshops.html')
+# @APP.route('/shops')
+# def coffeeshops() :
+#     return flask.render_template('coffeeshops.html', coffeeId1 = "1", name1 = "Summer Moon Coffee Bar", location1 = "11005 Burnet Rd Ste 112 Austin, TX 78758", price1 = "$", rating1 = "4.5", photo1 = "https://s3-media3.fl.yelpcdn.com/bphoto/WQPD9JYeDyVju0inUEID7w/o.jpg",
+#                                  name2 = "Houndstooth Coffee", coffeeId2 = "2", location2 = " 401 Congress AveSte 100C Austin, TX 78701", price2 = "$$", rating2 = "4.5", photo2 = "https://s3-media3.fl.yelpcdn.com/bphoto/ITv825S32-REV1bISyfk5A/o.jpg",
+#                                  name3 = "Vintage Heart Coffee", coffeeId3 = "3", location3 = "1405 E 7th St Austin, TX 78702", price3 = "$", rating3 = "4.5", photo3 = "https://s3-media3.fl.yelpcdn.com/bphoto/hK35KSh9IxFMjvvg4tCmsQ/o.jpg")
 
 # @APP.route('/scenic')
 # def home():
